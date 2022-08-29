b7329700dc8e9763f2afb7028c4d5f8509d47437
Victor Manuel Guayara Suarez <61199510+guayara504@users.noreply.github.com>
2022-08-29 15:08:45 -0500

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -25,7 +25,7 @@ fecha = time.strftime("%Y-%m-%d")
 fechaSegundos = time.strftime("%d-%m-%Y-(%H-%M-%S)")
 print (fecha)
 
-#try:
+
 print("-"*15)
 print("GENERADOR SQL")
 cursor1 = conexion1.cursor()
@@ -36,12 +36,14 @@ if len(consultasBD) == 0:
 else:
     id_z04 = []
     consultatxt = []
-    updateRemota = []
+    updateRemotaInsert = []
+    updateRemotaUpdate = []
+    updateRemotaInsert.append("INSERT INTO z04_estado (z01_radicacion_juzgado, z01_radicacion_z01_radicacion, demandante, demandado, notificacion, fecha_notificacion, clase_proceso, folio, cuaderno, ciudad) VALUES ")
     for consultaBD in consultasBD:  
       id_z04.append(consultaBD[0])
       juzgado = consultaBD[1]
       radicacion = consultaBD[2]
-      demandante = consultaBD[3]
+      demandante = consultaBD[3] 
       demandado = consultaBD[4]
       notificacion = consultaBD[5]
       fecha_notificacion = consultaBD[6]
@@ -52,8 +54,10 @@ else:
       
       consultatxt.append(f"""INSERT INTO z04_estado (z01_radicacion_juzgado, z01_radicacion_z01_radicacion, demandante, demandado, notificacion, fecha_notificacion, clase_proceso, folio, cuaderno, ciudad) VALUES ('{juzgado}','{radicacion}', '{demandante}', '{demandado}', '{notificacion}','{fecha_notificacion}','{clase_proceso}','{folio}','{cuaderno}','{ciudad}')
 UPDATE z01_radicacion_has_z02_abogado, z02_abogado SET z01_radicacion_has_z02_abogado.leido = 1 WHERE z01_radicacion_has_z02_abogado.z01_radicacion_z01_radicacion = '{radicacion}' AND z01_radicacion_has_z02_abogado.ciudad='{ciudad}' AND z01_radicacion_has_z02_abogado.z01_radicacion_juzgado = '{juzgado}' AND z01_radicacion_has_z02_abogado.z02_abogado_idcedula_z02=z02_abogado.idcedula_z02 AND z02_abogado.estado=1""")
-      updateRemota.append(f"INSERT INTO z04_estado (z01_radicacion_juzgado, z01_radicacion_z01_radicacion, demandante, demandado, notificacion, fecha_notificacion, clase_proceso, folio, cuaderno, ciudad) VALUES ('{juzgado}','{radicacion}', '{demandante}', '{demandado}', '{notificacion}','{fecha_notificacion}','{clase_proceso}','{folio}','{cuaderno}','{ciudad}')")
-      updateRemota.append(f"UPDATE z01_radicacion_has_z02_abogado, z02_abogado SET z01_radicacion_has_z02_abogado.leido = 1 WHERE z01_radicacion_has_z02_abogado.z01_radicacion_z01_radicacion = '{radicacion}' AND z01_radicacion_has_z02_abogado.ciudad='{ciudad}' AND z01_radicacion_has_z02_abogado.z01_radicacion_juzgado = '{juzgado}' AND z01_radicacion_has_z02_abogado.z02_abogado_idcedula_z02=z02_abogado.idcedula_z02 AND z02_abogado.estado=1")      
+
+
+      updateRemotaInsert.append(f"('{juzgado}','{radicacion}', '{demandante}', '{demandado}', '{notificacion}','{fecha_notificacion}','{clase_proceso}','{folio}','{cuaderno}','{ciudad}'),")
+      updateRemotaUpdate.append(f"UPDATE z01_radicacion_has_z02_abogado, z02_abogado SET z01_radicacion_has_z02_abogado.leido = 1 WHERE z01_radicacion_has_z02_abogado.z01_radicacion_z01_radicacion = '{radicacion}' AND z01_radicacion_has_z02_abogado.ciudad='{ciudad}' AND z01_radicacion_has_z02_abogado.z01_radicacion_juzgado = '{juzgado}' AND z01_radicacion_has_z02_abogado.z02_abogado_idcedula_z02=z02_abogado.idcedula_z02 AND z02_abogado.estado=1")      
 with open(f'.\\{ruta.dife_fecha()}\\{dia}\\{fechaSegundos}.txt','w') as temp_file:
     for item in consultatxt:
         temp_file.write("%s\n" % item)
@@ -67,16 +71,24 @@ print(id_z04,"---",len(id_z04))
 
 cursor1.execute(f"UPDATE z04_estado SET sincronizado = 1 WHERE id_z04_estado  IN {id_z04}")
 cursor1.close()
-print("\nSubiendo")
-n=0
+
+
+
+print("\nSubiendo\n----------------")
+print("Insertando Lineas...")
+
 cursor2 = conexion2.cursor()
-for consulta in updateRemota:
+updateRemotaInsert = "".join(updateRemotaInsert)
+updateRemotaInsert = updateRemotaInsert[:-1]
+cursor2.execute(updateRemotaInsert)
+print("¡Lineas Insertadas!")
+n=0
+print("Ejecutando UPDATE")
+for consulta in updateRemotaUpdate:
     n+=1
     print("Proceso: ",n)
     cursor2.execute(consulta)
-
-#except:
-   # print("\nNo Hubo conexion con la base de datos...")
+print("UPDATE Ejecutado\n-----------------\n")
 
 
 
